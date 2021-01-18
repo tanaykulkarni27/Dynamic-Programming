d81ca0ad132a46a096876b17b503fe93f8ac7a5a
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2021-01-18 22:02:18 +0530

    Update knap.py

--- knap.py
+++ knap.py
@@ -4,35 +4,30 @@ def read_int_arr():
     return [int(i) for i in input().split()]
 def read_str_arr():
     return [i for i in input().split()]
-# Knapsack problem with  memoization
-def knap(weights,values,capacity,index,memo):
-    if index == -1 or capacity <= 0:
-        return 0
-    elif memo[index][capacity] != -1:
-        print(memo)
-        return memo[index][capacity]
-    else:
-        temp = knap(weights,values,capacity,index-1,memo)
-        temp_2 = values[index]+knap(weights,values,capacity-weights[index],index-1,memo)
-        memo[index][capacity] = max(temp_2,temp)
-        return memo[index][capacity]
+def knap(wt,vals,cap):
+    dp = [[-1 for i in range(cap+1)] for i in range(len(wt)+1)]
+    for i in range(len(wt)+1):
+        for j in range(cap+1):
+            if i == 0 or j <= 0:
+                dp[i][j] = 0
+            elif wt[i-1]<=cap:
+                dp[i][j] = max(vals[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j])
+            else:
+                dp[i][j] = dp[i-1][j]
+    return dp[len(wt)][cap]
 def solve():
     n,c = read_int_arr()
-    weights = read_int_arr()
-    values = read_int_arr()
-    dp = [[-1 for i in range(c+1)]for i in range(n+1)]
-    ans =  knap(weights,values,c,n-1,dp)
-    print(dp)
-    return ans
+    wt = read_int_arr()
+    prc = read_int_arr()
+    return knap(wt,prc,c)
 t = read_int()
 for _ in range(1,t+1):
     ans = solve()
     print("Case #{}:".format(_),end=' ')
     print(ans)
 '''
-1   
+1
 3 50
 10 20 30
 60 100 120
-
 '''
